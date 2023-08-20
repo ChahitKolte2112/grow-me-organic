6580fb5b7cb1203e9adf80b8ec4c6a155fbc0412
ChahitKolte <chahitkolte@gmail.com>
2023-08-20 13:36:26 +0530

    data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,69 @@
 import "./App.css";
+import {useEffect, useState} from "react"
+import axios from "axios";
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
+
+interface Post {
+  userId : string,
+  id : string,
+  title:string,
+  body : string,
+}
 function App() {
-  return <> hello world</>
+  const [data ,setData]   = useState<Post[]>([]);
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'id', width: 50 },
+    {
+      field: 'userId',
+      headerName: 'userId',
+      width: 50,
+      editable: false,
+    },
+    {
+      field: 'title',
+      headerName: 'title',
+      width: 50,
+      editable: true,
+    },
+    {
+      field: 'body',
+      headerName: 'body',
+      type: 'string',
+      width: 400,
+      editable: false,
+    }
+  ];
+
+  useEffect(()=>{
+    const fetchData : any = async ()=>{
+      const result : any = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      console.log(result)
+      setData(result?.data);
+    }
+    fetchData();
+  },[])
+
+  
+  return <>
+    <Box sx={{ height: 400, width: '50%' }}>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          disableRowSelectionOnClick
+        />
+      </Box>
+  </>
 }
 
 export default App;
